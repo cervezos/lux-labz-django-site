a8d4489f62681a7524ee347e9ffee396472eafdd
cervezos <joe.zapp@gmail.com>
2024-04-14 01:28:01 -0700

    Added additional navbar items; Added About Us page

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fz6+lw%5*b^&j8&wz5vb-xi-+2^$klpagds6r(cag^8*@xno83'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+# [JVZ] When set to false, unable to locate the css file
 
 ALLOWED_HOSTS = ['*']
 
